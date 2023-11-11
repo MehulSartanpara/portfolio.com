2a2d936832a4833caac83d2da82837049893c3fd
Mehul Sartanpara <sartanparamehul@gmail.com>
2023-11-11 23:49:48 +0530

    Responsive in progress

--- js/custom.js
+++ js/custom.js
@@ -20,11 +20,16 @@ $( document ).ready(function() {
   
   // Background Effect
   const cursor = document.querySelector('.cursor-position');
-  document.addEventListener('mousemove', (e) => {
-    const bgX = e.clientX - cursor.offsetWidth / 2;
-    const bgY = e.clientY - cursor.offsetHeight / 2;
-    cursor.style.backgroundPosition = `${bgX}px ${bgY}px`;
-  });
+  const windowSize = $(window).width();
+  if (windowSize >= 901) {
+    document.addEventListener('mousemove', (e) => {
+      const bgX = e.clientX - cursor.offsetWidth / 2;
+      const bgY = e.clientY - cursor.offsetHeight / 2;
+      cursor.style.backgroundPosition = `${bgX}px ${bgY}px`;
+    });
+  }else{
+    cursor.remove();
+  }
 
   // Section Tab Active
   const sections = document.querySelectorAll("section[id]");
